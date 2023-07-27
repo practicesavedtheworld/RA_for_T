7bc1536702fa4599f52e2cced5068cc586bb46ed
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-07-27 12:23:44 +0300

    add router users

--- app/backend/users/router.py
+++ app/backend/users/router.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
 from starlette.responses import Response
 
+from app.backend.exceptions.user_exceptions import WrongUsernameOrPassword, UsernameAlreadyTaken
 from app.backend.users.auth import create_hashed_password, authenticate_user, generate_token
 from app.backend.users.dao import UsersDAO
+from app.backend.users.dependencies import get_current_user
+from app.backend.users.models import Users
 from app.backend.users.schemas import UsersScheme
 
 router = APIRouter(
@@ -21,9 +24,7 @@ async def new_user_registration(user: UsersScheme):
         hashed_pass = create_hashed_password(user.non_hashed_password)
         await UsersDAO.add(username=user.username, hashed_password=hashed_pass)
     else:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-        )
+        raise UsernameAlreadyTaken
 
 
 @router.post('/login')
@@ -33,15 +34,19 @@ async def login(abc_user: UsersScheme, response: Response):
     user_exist = await UsersDAO.get_by_name(abc_user.username)
     if not user_exist:
         # TODO add custom exp
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-        )
-    user_passed_validation: bool = await authenticate_user(user_exist.username, abc_user.non_hashed_password)
-    if user_passed_validation:
-        user_token = generate_token({"sub": user_exist.id})
+        raise WrongUsernameOrPassword
+
+    user_that_passed_validation: bool = await authenticate_user(user_exist.username, abc_user.non_hashed_password)
+    if user_that_passed_validation:
+        user_token = generate_token({"sub": str(user_exist.id)})
         response.set_cookie(
             'targets_user_token',
             user_token,
             httponly=True,
         )
-    return "EST", user_passed_validation
+    return user_that_passed_validation
+
+
+@router.post('/me')
+async def myself(user: Users = Depends(get_current_user)):
+    return user
