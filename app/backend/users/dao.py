9c357da15c1363fa044f439531d209a7bf211d8b
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-07-28 15:28:19 +0300

    update usersdao

--- app/backend/users/dao.py
+++ app/backend/users/dao.py
@@ -17,11 +17,5 @@ class UsersDAO(BaseDAO):
             user_model: Users = query_res.scalar_one_or_none()
             return user_model
 
-    @classmethod
-    async def get_by_id(cls, id_: int) -> current_model:
-        async with cls.session_ as session:
-            select_query = select(cls.current_model).filter_by(id=id_)
-            query_result = await session.execute(select_query)
-            return query_result.scalar_one_or_none()
 
 
