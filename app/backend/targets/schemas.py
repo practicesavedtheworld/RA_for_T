7d8b872519abbd4207d64867adb4b64417d25e30
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-07-28 15:54:44 +0300

    add targets schemas

---
+++ app/backend/targets/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+
+class RawTarget(BaseModel):
+    title: str = Field(max_length=150)
+    description: str = Field(max_length=300)
+    status: str = Field(default='new')
+
+
+class DetailedTarget(RawTarget):
+    id: int
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+
+
+class UpdatedTarget(DetailedTarget):
+    updated_at: datetime
