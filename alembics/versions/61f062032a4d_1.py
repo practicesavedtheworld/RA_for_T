6587e6e046c04edf2a02490550c624df16e0f956
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-07-28 15:36:57 +0300

    add alembic migrations folder

---
+++ alembics/versions/61f062032a4d_1.py
@@ -0,0 +1,24 @@
+"""1
+
+Revision ID: 61f062032a4d
+Revises: 
+Create Date: 2023-07-25 12:02:51.682949
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '61f062032a4d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
