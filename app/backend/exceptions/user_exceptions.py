82674378bc2cfd80aaa5ae9031ccfd51e11e1715
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-07-28 15:38:47 +0300

    fix status

--- app/backend/exceptions/user_exceptions.py
+++ app/backend/exceptions/user_exceptions.py
@@ -1,9 +1,9 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 
 
 class BaseUserException(HTTPException):
     details = ''
-    exception_status_code = 500
+    exception_status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
 
     def __init__(self):
         super().__init__(detail=self.details, status_code=self.exception_status_code)
