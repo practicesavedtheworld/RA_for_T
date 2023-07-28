99bfc94305145eac6d798567bd92be7578f053be
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-07-28 15:52:23 +0300

    fix default value, add status

--- app/backend/targets/models.py
+++ app/backend/targets/models.py
@@ -13,8 +13,9 @@ class Targets(Base):
     user_id: Mapped[int] = Column(ForeignKey('users.id'), nullable=False, type_=Integer)
     title: Mapped[str] = Column(type_=String(50), nullable=False)
     description: Mapped[str] = Column(type_=String(200))
+    status: Mapped[str] = Column(type_=String(20), nullable=False, default='new')
     created_at: Mapped[DateTime] = Column(type_=DateTime, nullable=False, default=datetime.utcnow)
-    updated_at: Mapped[DateTime] = Column(type_=DateTime, nullable=True, default=Null)
-    deleted_at: Mapped[DateTime] = Column(type_=DateTime, nullable=True, default=Null)
+    updated_at: Mapped[DateTime] = Column(type_=DateTime, nullable=True, default=datetime.utcnow)
+    deleted_at: Mapped[DateTime] = Column(type_=DateTime, nullable=True, default=datetime.utcnow)
 
     user = relationship('Users', back_populates='targets')
