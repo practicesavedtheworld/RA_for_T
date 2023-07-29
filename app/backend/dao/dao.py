e73044035fdd9aa28923b7607e861c382cd3d90c
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-07-29 12:08:28 +0300

    add delete_taask_by_ids

--- app/backend/dao/dao.py
+++ app/backend/dao/dao.py
@@ -1,10 +1,8 @@
-from typing import Sequence
-
 from app.backend.database import asynch_session, Base
-from sqlalchemy.ext.asyncio import AsyncSession, AsyncSessionTransaction, AsyncConnection
-from sqlalchemy import select, insert, delete, and_, or_, Result, Row
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, insert, delete
 
-
+from app.backend.users.exceptions import NoUserFound
 
 
 class BaseDAO:
@@ -25,3 +23,13 @@ class BaseDAO:
             select_query = select(cls.current_model).filter_by(id=user_id)
             query_result = await session.execute(select_query)
             return query_result.scalar_one_or_none()
+
+    @classmethod
+    async def delete_task_by_task_id(cls, task_id: int, user_id: int | None = None):
+        if not user_id:
+            raise NoUserFound
+        async with cls.session_ as session:
+            delete_query = delete(cls.current_model).filter_by(id=task_id, user_id=user_id).returning(cls.current_model)
+            delete_query_result = await session.execute(delete_query)
+            await session.commit()
+            return delete_query_result.scalar()
