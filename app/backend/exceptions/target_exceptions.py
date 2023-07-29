3e5f091ca23840bfc0c3688ad0920a89e6aa50fd
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-07-29 12:09:12 +0300

    add base for target exceptions

---
+++ app/backend/exceptions/target_exceptions.py
@@ -0,0 +1,10 @@
+from fastapi import status, HTTPException
+
+
+class BaseTargetException(HTTPException):
+    detail = 'Internal server error'
+    exception_status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+    def __init__(self):
+        super().__init__(status_code=self.exception_status_code, detail=self.detail)
+
