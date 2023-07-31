36e8ffc196d4b5361189d84ca4573effb942318d
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-07-31 12:04:32 +0300

    fix missed type for id

--- app/backend/users/models.py
+++ app/backend/users/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import Mapped, relationship
 
 from app.backend.database import Base
@@ -7,7 +7,7 @@ from app.backend.database import Base
 class Users(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = Column(primary_key=True, nullable=False)
+    id: Mapped[int] = Column(Integer, primary_key=True, nullable=False)
     username: Mapped[str] = Column(type_=String(50))
     hashed_password: Mapped[str]
 
