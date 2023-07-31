718de8516579c73315cb3d922c64f7e5522f61e4
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-07-31 16:21:11 +0300

    now db_url depends on mode

--- app/backend/database.py
+++ app/backend/database.py
@@ -3,10 +3,16 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.backend.configurations import settings
 
+if settings.MODE == "TEST":
+    DB_URL = settings.TEST_DB_URL
+
+else:
+    DB_URL = settings.DB_URL
+
 
 class Base(DeclarativeBase):
     pass
 
 
-engine: AsyncEngine = create_async_engine(settings.DB_URL)
+engine: AsyncEngine = create_async_engine(DB_URL)
 asynch_session = async_sessionmaker(bind=engine, expire_on_commit=False)
