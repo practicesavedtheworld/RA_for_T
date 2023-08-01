97da9ffcd7fd1c9b657a51e8c27e6d9efb2d0c39
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-08-01 21:16:25 +0300

    add test-utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,14 +1,28 @@
 from faker import Faker
 
+from app.backend.targets.schemas import RawTarget
 from app.backend.users.schemas import UsersScheme
 
 fake = Faker()
 
 
-def get_fake_user():
+def get_fake_user() -> UsersScheme:
     fake_user_as_dict = {
         "username": fake.user_name(),
         "non_hashed_password": fake.password(),
     }
 
     return UsersScheme(**fake_user_as_dict)
+
+
+def get_fake_raw_target(
+        title: str = fake.word(),
+        description: str = fake.text(max_nb_chars=250),
+        status: str = "new",
+) -> RawTarget:
+    target = {
+        "title": title,
+        "description": description,
+        "status": status,
+    }
+    return RawTarget(**target)
