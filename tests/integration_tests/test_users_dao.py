4dc0519e23ad36416ba2a14d1bd7742fbffcf54a
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-08-01 21:44:54 +0300

    update TargetsDAO tests

--- tests/integration_tests/test_users_dao.py
+++ tests/integration_tests/test_users_dao.py
@@ -1,21 +1,44 @@
-from app.backend.targets.schemas import RawTarget
+from app.backend.targets.dao import TargetsDAO
+
+from tests.utils import get_fake_raw_target
 
 
-class TestAuthenticatedUserDAOManipulations:
-    """
+class TestAuthenticatedUserTargetsDAOManipulations:
+    """Basic CRUD DAO testing"""
 
-class RawTarget(BaseModel):
-    title: str = Field(max_length=150)
-    description: str = Field(max_length=300)
-    status: str = Field(default="new")
-    """
+    async def test_new_task(self, test_database_preparation, authenticated_user_session):
+        resp = await authenticated_user_session.post(
+            url='/tasks',
+            json=get_fake_raw_target().model_dump(),
+        )
 
-    async def test_new_task(self, test_database_preparation, authenticated_user):
-        session, fake_user = authenticated_user
-        resp = await session.post(url='/tasks', json=RawTarget(**{
-            "title": "qqqq",
-            "description": "qweqwe",
-            "status": "dodododo",
-        }).model_dump())
+        assert resp.status_code == 200
+
+    async def test_update_task(self, authenticated_user_session):
+        task_id = await TargetsDAO.find_all_raw_task(get_fake_raw_target())
+        resp = await authenticated_user_session.put(
+            url=f'/tasks/{task_id}',
+            params={"task_id": int(task_id)},
+            json=get_fake_raw_target(
+                title='test',
+                description='test',
+                status='done',
+            ).model_dump()
+        )
+
+        assert resp.status_code == 200
+
+    async def test_remove_task(self, authenticated_user_session):
+        task_id = await TargetsDAO.find_all_raw_task(get_fake_raw_target(
+            title='test',
+            description='test',
+            status='done',
+        ))
+        resp = await authenticated_user_session.delete(
+            url=f'/tasks/{task_id}',
+            params={
+                "task_id": int(task_id),
+            }
+        )
 
         assert resp.status_code == 200
